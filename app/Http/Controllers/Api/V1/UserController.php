8755d9370dccb18e3d214cc66e26947af3472f24
Brux Romuar <brux@ingenuity.ph>
2016-12-19 03:28:49 +0800

    Add user info endpoint.

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -1,11 +1,30 @@
 <?php namespace App\Http\Controllers\Api\V1;
 
+use Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
 
-    
+    /**
+     * Returns the current user's info.
+     * 
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function get(Request $request)
+    {
+        $user = $request->user();
+        $response = [
+            'name'          => $user->name,
+            'email'         => $user->email,
+            'info'          => $user->info,
+            'lang'          => $user->lang,
+            'navColor'      => $user->navColor,
+            'registeredOn'  => $user->created_at->toIso8601String()
+        ];
+        return response()->json($response);
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -1,2 +1,3 @@
 <?php
 
+Route::get('/user', 'UserController@get');
