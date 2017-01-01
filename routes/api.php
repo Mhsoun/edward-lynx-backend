0a9a5d4f3265714c058a1e0da4124439dfcfd0fd
Brux Romuar <brux@ingenuity.ph>
2017-01-02 03:32:55 +0800

    Group /surveys endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,11 @@
 Route::get('/user', 'UserController@get');
 Route::patch('/user', 'UserController@update');
 
-Route::get('/surveys', 'SurveyController@index');
-Route::post('/surveys', 'SurveyController@create');
-Route::get('/surveys/{survey}', 'SurveyController@show')
-    ->middleware('can:view,survey')
-    ->name('api1-survey');
+// /surveys Endpoints
+Route::group(['prefix' => '/surveys'], function() {
+    Route::get('/', 'SurveyController@index');
+    Route::post('/', 'SurveyController@create');
+    Route::get('/surveys/{survey}', 'SurveyController@show')
+        ->middleware('can:view,survey')
+        ->name('api1-survey');
+});
