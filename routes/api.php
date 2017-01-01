0807297d13b48f1bbbe6467f8703ab614ed7b5ff
Brux Romuar <brux@ingenuity.ph>
2017-01-02 03:44:31 +0800

    Remove extra /surveys prefix for route.

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ Route::group(['prefix' => 'user'], function() {
 Route::group(['prefix' => '/surveys'], function() {
     Route::get('/', 'SurveyController@index');
     Route::post('/', 'SurveyController@create');
-    Route::get('/surveys/{survey}', 'SurveyController@show')
+    Route::get('/{survey}', 'SurveyController@show')
         ->middleware('can:view,survey')
         ->name('api1-survey');
 });
