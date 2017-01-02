d300dbb4ab711b578a97ab2c0bdbf7d9b73e17be
Brux Romuar <brux@ingenuity.ph>
2017-01-02 15:04:47 +0800

    Add json-hal support.

---
+++ app/Providers/HalServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\HalResponse;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Routing\ResponseFactory;
+
+class HalServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot(ResponseFactory $response)
+    {
+        $response->macro('hal', function($input, $status = 200, $headers = [], $options = 0) {
+            return new HalResponse($input, $status, $headers, $options);
+        });
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
