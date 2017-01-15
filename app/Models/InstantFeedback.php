1fc15420ff1b95202a63266e76fd7741c7fde40e
Brux Romuar <brux@ingenuity.ph>
2017-01-16 07:56:13 +0800

    Include instant feedback questions.

--- app/Models/InstantFeedback.php
+++ app/Models/InstantFeedback.php
@@ -12,6 +12,16 @@ class InstantFeedback extends Model
     protected $visible = ['id', 'lang', 'closed', 'anonymous'];
     
     /**
+     * Returns this instant feedback's questions.
+     *
+     * @return  Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function questions()
+    {
+        return $this->belongsToMany('App\Models\Question', 'instant_feedback_questions');
+    }
+    
+    /**
      * Overrides our JSON representation and adds a createdAt field
      *
      * @return array
@@ -20,6 +30,7 @@ class InstantFeedback extends Model
     {
         $data = parent::jsonSerialize();
         $data['createdAt'] = $this->created_at->toIso8601String();
+        $data['questions'] = $this->questions;
         return $data;
     }
     
