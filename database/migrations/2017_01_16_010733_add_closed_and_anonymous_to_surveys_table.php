4e880f08cac95020b5556ddc16531e4b582a1885
Brux Romuar <brux@ingenuity.ph>
2017-01-16 08:12:43 +0800

    Move instant feedbacks to surveys.

--- database/migrations/2017_01_16_010733_add_closed_and_anonymous_to_surveys_table.php
+++ database/migrations/2017_01_16_010733_add_closed_and_anonymous_to_surveys_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateInstantFeedbacksTable extends Migration
+class AddClosedAndAnonymousToSurveysTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,9 @@ class CreateInstantFeedbacksTable extends Migration
      */
     public function up()
     {
-        Schema::create('instant_feedbacks', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->unsigned();
-            $table->string('lang');
+        Schema::table('surveys', function (Blueprint $table) {
             $table->boolean('closed')->default(false);
             $table->boolean('anonymous')->default(false);
-            $table->timestamps();
-            
-            $table->foreign('user_id')
-                  ->references('id')->on('users')
-                  ->onDelete('cascade');
         });
     }
 
@@ -34,6 +26,9 @@ class CreateInstantFeedbacksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('instant_feedbacks');
+        Schema::table('surveys', function (Blueprint $table) {
+            $table->dropColumn('closed');
+            $table->dropColumn('anonymous');
+        });
     }
 }
