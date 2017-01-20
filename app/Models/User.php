2fae39743317f3999d76ceadc34c5cf0148f2e84
Brux Romuar <brux@ingenuity.ph>
2017-01-20 13:13:05 +0800

    Remove commented out imports

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,12 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-// use Illuminate\Auth\Authenticatable;
-// use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-// use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Lang;
 use App\Models\DefaultText;
 
