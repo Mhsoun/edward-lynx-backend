328df660868b9c59fd21d8e90a42a558f165e248
Brux Romuar <brux@ingenuity.ph>
2017-01-25 23:17:59 +0800

    Add a scope for filtering due goals.

--- app/Models/DevelopmentPlanGoal.php
+++ app/Models/DevelopmentPlanGoal.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use App\Models\BaseModel;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
 use Illuminate\Database\Eloquent\Scope;
@@ -11,11 +12,27 @@ use Illuminate\Database\Eloquent\Builder;
 class DevelopmentPlanGoal extends BaseModel implements Scope
 {
     
+    const DUE_THRESHOLD = 2;
+    
     public $fillable = ['title', 'description', 'checked', 'position', 'dueDate'];
     
     public $timestamps = false;
     
     protected $dates = ['dueDate'];
+
+    /**
+     * Scopes results to goals that are within the due date threshold.
+     *
+     * @param   Illuminate\Database\Eloquent\Builder    $query
+     * @return  Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeDue(Builder $query)
+    {
+        $now = Carbon::now();
+        $due = Carbon::now()->addDays(self::DUE_THRESHOLD);
+        return $query->whereDate('dueDate', '>=', $now)
+                     ->whereDate('dueDate', '<=', $due);
+    }
     
     /**
      * Returns the development plan this goal is under.
