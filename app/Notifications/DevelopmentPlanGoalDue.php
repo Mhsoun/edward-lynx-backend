780a7414d9a531e5ef33834e49f7b806dec73b14
Brux Romuar <brux@ingenuity.ph>
2017-01-25 23:36:36 +0800

    Queue reminders so it won’t bog down the runner.

--- app/Notifications/DevelopmentPlanGoalDue.php
+++ app/Notifications/DevelopmentPlanGoalDue.php
@@ -10,7 +10,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class DevelopmentPlanGoalDue extends Notification
+class DevelopmentPlanGoalDue extends Notification implements ShouldQueue
 {
     use Queueable;
 
