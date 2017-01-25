01751cda0363be807cb7efa17cbe0b5a16f2e2cf
Brux Romuar <brux@ingenuity.ph>
2017-01-25 21:30:54 +0800

    Sort goals by their position

--- app/Models/DevelopmentPlanGoal.php
+++ app/Models/DevelopmentPlanGoal.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Builder;
 
-class DevelopmentPlanGoal extends Model
+
+class DevelopmentPlanGoal extends Model implements Scope
 {
     
     public $fillable = ['title', 'description', 'checked', 'position', 'dueDate'];
@@ -19,6 +22,18 @@ class DevelopmentPlanGoal extends Model
     public function developmentPlan()
     {
         return $this->belongsTo(DevelopmentPlan::class, 'developmentPlanId');
+    }
+    
+    /**
+     * Goals are sorted by their position by default.
+     *
+     * @param   Illuminate\Database\Eloquent\Builder    $builder
+     * @param   Illuminate\Database\Eloquent\Model      $model
+     * @return  void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->orderBy('position', 'asc');
     }
     
 }
