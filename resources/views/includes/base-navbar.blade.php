f7c1f7af34d07525760e6deeb728b89c631eb71c
Brux Romuar <brux@ingenuity.ph>
2017-05-26 14:49:18 +0800

    Fix dark navbar.

--- resources/views/includes/base-navbar.blade.php
+++ resources/views/includes/base-navbar.blade.php
@@ -6,7 +6,7 @@
 ?>
 
 <header class="navbar navbar-inverse navbar-fixed-top" id="navColor" role="banner"
-        style="background-color: #{{ $user != null && !$user->isAdmin ? $user->navColor : 'DDDDDD' }}">
+        style="background-color: #{{ $user != null && !$user->isAdmin && $user->navColor ? $user->navColor : 'DDDDDD' }}">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
