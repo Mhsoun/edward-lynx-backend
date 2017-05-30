adea099cb20cd1c374c595f054e6c2117a59b06a
Brux Romuar <brux@ingenuity.ph>
2017-05-30 17:12:56 +0800

    Use correct namespace.

--- app/Http/Controllers/Api/V1/TestController.php
+++ app/Http/Controllers/Api/V1/TestController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Model\User;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Notifications\TestNotification;
