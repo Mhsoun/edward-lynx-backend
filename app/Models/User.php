d89150ddcb2eef541f0303bcb8e889c293355570
Brux Romuar <brux@ingenuity.ph>
2017-06-14 14:42:58 +0800

    Sort by goal due date.

--- app/Models/User.php
+++ app/Models/User.php
@@ -913,7 +913,7 @@ class User extends Authenticatable implements AuthorizableContract, Routable
 
         $sorted = $collection->sortBy(function ($item) {
             if ($item instanceof DevelopmentPlanGoal) {
-                return $item->developmentPlan->createdAt->timestamp;
+                return $item->developmentPlan->dueDate->timestamp;
             } elseif ($item instanceof InstantFeedback) {
                 return $item->createdAt->timestamp;
             } elseif ($item instanceof Survey) {
