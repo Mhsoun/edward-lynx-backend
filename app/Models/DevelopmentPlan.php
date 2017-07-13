d842ca7fc336e1641b69ca5d7dad0bae9f84377f
Brux Romuar <brux@ingenuity.ph>
2017-07-13 13:22:36 +0800

    Remove team details.

--- app/Models/DevelopmentPlan.php
+++ app/Models/DevelopmentPlan.php
@@ -10,9 +10,6 @@ use Illuminate\Database\Eloquent\Builder;
 
 class DevelopmentPlan extends BaseModel implements Routable, JsonHalLinking
 {
-    
-    const SERIALIZE_NORMAL = 0;
-    const SERIALIZE_TEAM_DETAILS = 2;
 
     const CREATED_AT = 'createdAt';
     const UPDATED_AT = 'updatedAt';
@@ -20,58 +17,6 @@ class DevelopmentPlan extends BaseModel implements Routable, JsonHalLinking
     public $fillable = ['name', 'shared'];
     
     protected $visible = ['id', 'name', 'checked', 'shared', 'createdAt', 'updatedAt'];
-
-    /**
-     * Sort team development plans by position.
-     * 
-     * @param   App\Models\User  $owner
-     * @return  void
-     */
-    public static function sortTeamsByPosition(User $owner)
-    {
-        $devPlans = $owner->developmentPlans()
-                          ->forTeams()
-                          ->orderBy('position', 'ASC')
-                          ->get();
-
-        foreach ($devPlans as $index => $devPlan) {
-            $devPlan->updateTeamAttribute(['position' => $index]);
-        }
-    }
-
-    /**
-     * Creates a team development plan.
-     * 
-     * @param   App\Models\DevelopmentPlan   $devPlan
-     * @return  App\Models\DevelopmentPlan
-     */
-    public static function insertAsTeamDevelopmentPlan(DevelopmentPlan $devPlan)
-    {
-        if ($devPlan->isTeam()) {
-            return $devPlan;
-        }
-
-        $devPlans = DB::table('development_plan_team_attributes')
-                        ->select('development_plans.id', 'development_plan_team_attributes.position')
-                        ->join('development_plans', 'development_plan_team_attributes.developmentPlanId', '=', 'development_plans.id')
-                        ->where('development_plans.ownerId', $devPlan->ownerId)
-                        ->get();
-
-        foreach ($devPlans as $dp) {
-            DB::table('development_plan_team_attributes')
-                ->where('developmentPlanId', $dp->id)
-                ->update(['position' => $dp->position + 1]);
-        }
-
-        DB::table('development_plan_team_attributes')
-            ->insert([
-                'developmentPlanId' => $devPlan->id,
-                'position'          => 0,
-                'visible'           => false
-            ]);
-
-        return $devPlan;
-    }
 
     /**
      * Returns the API url to this development plan.
@@ -106,18 +51,6 @@ class DevelopmentPlan extends BaseModel implements Routable, JsonHalLinking
     {
         return $query->where('checked', false);
     }
-
-    /**
-     * Scopes results to team development plans only.
-     * 
-     * @param   Illuminate\Database\Eloquent\Builder    $query
-     * @return  Illuminate\Database\Eloquent\Builder
-     */
-    public function scopeForTeams(Builder $query)
-    {
-        $query->join('development_plan_team_attributes', 'development_plans.id', '=', 'development_plan_team_attributes.developmentPlanId');
-        return $query->orderBy('development_plan_team_attributes.position', 'ASC');
-    }
     
     /**
      * Returns the user who owns this development plan.
@@ -138,41 +71,6 @@ class DevelopmentPlan extends BaseModel implements Routable, JsonHalLinking
     {
         return $this->hasMany(DevelopmentPlanGoal::class, 'developmentPlanId')
                     ->orderBy('position', 'asc');
-    }
-
-    /**
-     * Returns the team attribute record for this model.
-     * 
-     * @return  object|null
-     */
-    public function teamAttribute()
-    {
-        return DB::table('development_plan_team_attributes')
-                    ->where('developmentPlanId', $this->id)
-                    ->first();
-    }
-
-    /**
-     * Returns TRUE if this is a team development plan.
-     * 
-     * @return  boolean
-     */
-    public function isTeam()
-    {
-        return $this->teamAttribute() !== null;
-    }
-
-    /**
-     * Updates team attributes.
-     * 
-     * @param   array  $attributes
-     * @return  void
-     */
-    public function updateTeamAttribute(array $attributes)
-    {
-        DB::table('development_plan_team_attributes')
-            ->where('developmentPlanId', $this->id)
-            ->update($attributes);
     }
 
     /**
@@ -238,73 +136,9 @@ class DevelopmentPlan extends BaseModel implements Routable, JsonHalLinking
      */
     public function jsonSerialize($type = 0)
     {
-        switch ($type) {
-            case self::SERIALIZE_NORMAL:
-                return $this->jsonSerializeNormal();
-                break;
-            case self::SERIALIZE_TEAM_DETAILS:
-                return $this->jsonSerializeTeamDetailed();
-                break;
-        }
-    }
-
-    /**
-     * Serialize as a normal development plan.
-     * 
-     * @return  array
-     */
-    public function jsonSerializeNormal()
-    {
         $json = parent::jsonSerialize();
         $json['goals'] = $this->goals;
         return $json;
-    }
-
-    /**
-     * Serialize as a team development plan.
-     * 
-     * @return  array
-     */
-    public function jsonSerializeTeamDetailed()
-    {
-        $goalsByUser = [];
-        foreach ($this->goals as $goal) {
-            if (!isset($goalsByUser[$goal->owner->id])) {
-                $goalsByUser[$goal->owner->id] = [];
-            }
-
-            $goalsByUser[$goal->owner->id][] = $goal;
-        }
-
-        $goals = [];
-        foreach ($goalsByUser as $userId => $goalsColl) {
-            $user = User::find($userId);
-            $goals[] = [
-                'id'    => $user->id,
-                'name'  => $user->name,
-                'goals' => array_map(function($goal) {
-                    return [
-                        'title'     => $goal->title,
-                        'progress'  => $goal->calculateProgress(),
-                        'actions'   => $goal->actions
-                    ];
-                }, $goalsColl)
-            ];
-        }
-
-        return [
-            '_links'    => [
-                'self'  => ['href' => route('api1-dev-plan-manager-teams.show', $this)],
-                'goals' => ['href' => route('api1-dev-plan-goals.index', $this)]
-            ],
-            'id'        => $this->id,
-            'name'      => $this->name,
-            'ownerId'   => $this->ownerId,
-            'position'  => $this->position,
-            'checked'   => $this->checked,
-            'visible'   => $this->attributes['visible'],
-            'goals'     => $goals
-        ];
     }
     
 }
