0f83f335c1ba60ba29f0344f42fbdbabbc3bbba7
Brux Romuar <brux@ingenuity.ph>
2017-08-30 15:23:42 +0800

    Drop instant feedback recipient foreign key if it exists.

---
+++ database/migrations/2017_08_30_090602_remove_instant_feedback_recipient_user_id_foreign_eys.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveInstantFeedbackRecipientUserIdForeignEys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('instant_feedback_recipients', function (Blueprint $table) {
+            if ($this->hasForeignKey('instant_feedback_recipients', 'instant_feedback_recipients_user_id_foreign')) {
+                $table->dropForeign('instant_feedback_recipients_user_id_foreign');
+            }
+            if ($this->hasForeignKey('instant_feedback_recipients', 'instant_feedback_recipients_userid_foreign')) {
+                $table->dropForeign('instant_feedback_recipients_userid_foreign');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('instant_feedback_recipients', function (Blueprint $table) {
+            $table->foreign('recipientId')
+                  ->refereces('id')->on('users')
+                  ->onDelete('cascade');
+        });
+    }
+
+    protected function hasForeignKey($table, $key)
+    {
+        $foreignKeys = Schema::getConnection()->getDoctrineSchemaManager()->listTableForeignKeys($table);
+        $foreignKeys = array_map(function ($foreignKey) {
+            return $foreignKey->getName();
+        }, $foreignKeys);
+        return in_array($key, $foreignKeys);
+    }
+}
