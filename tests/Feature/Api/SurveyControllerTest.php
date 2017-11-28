9986840574f5dfd79fd7b878f9705b61ea126a4c
Brux Romuar <brux.romuar@gmail.com>
2017-11-28 13:17:52 +0800

    Add factory methods.

---
+++ tests/Feature/Api/SurveyControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Survey;
+use Tests\Helpers\SurveyHelper;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class SurveyControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    
+}
