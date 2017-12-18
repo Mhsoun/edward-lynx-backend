d8f69e66b5793b971e300e39ec14a25539cc29d3
Brux Romuar <brux@ingenuity.ph>
2017-12-18 22:14:56 +0800

    Add unit tests for surveys.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -9,31 +9,6 @@ $factory->define(App\Models\EmailText::class, function (Faker\Generator $faker) 
     ];
 });
 
-$factory->define(App\Models\Survey::class, function (Faker\Generator $faker) use ($factory) {
-    return [
-        'name'                  => $faker->words(3, true),
-        'type'                  => 'App\SurveyTypes::Individual',
-        'lang'                  => 'en',
-        'invitationTextId'      => $factory->create(App\Models\EmailText::class)->id,
-        'ownerId'               => 1,
-        'startDate'             => Carbon\Carbon::now(),
-        'endDate'               => Carbon\Carbon::now()->addDay(30),
-        'description'           => $faker->words(15, true),
-        'thankYouText'          => $faker->words(15, true),
-        'questionInfoText'      => $faker->words(15, true),
-    ];
-});
-
-$factory->define(App\Models\QuestionCategory::class, function (Faker\Generator $faker) use ($factory) {
-    return [
-        'title'         => $faker->words(3, true),
-        'lang'          => 'en',
-        'description'   => $faker->sentences(3, true),
-        'ownerId'       => 1,
-        'isSurvey'      => true
-    ];
-});
-
 $factory->define(App\Models\DevelopmentPlan::class, function (Faker\Generator $faker) {
     return [
         'ownerId'       => 1,
@@ -61,12 +36,67 @@ $factory->define(App\Models\DevelopmentPlanGoalAction::class, function (Faker\Ge
     ];
 });
 
+$factory->define(App\Models\Question::class, function (Faker\Generator $faker) use ($factory) {
+    return [
+        'text'          => $faker->words(5, true),
+        'ownerId'       => function() {
+            return factory(App\Models\User::class)->create()->id;
+        },
+        'categoryId'    => function() {
+            return factory(App\Models\QuestionCategory::class)->create()->id;   
+        },
+        'answerType'    => 0,
+    ];
+});
+
+$factory->define(App\Models\QuestionCategory::class, function (Faker\Generator $faker) use ($factory) {
+    return [
+        'title'         => $faker->words(3, true),
+        'lang'          => 'en',
+        'description'   => $faker->sentences(3, true),
+        'ownerId'       => function() {
+            return factory(App\Models\User::class)->create([
+                'isAdmin'       => true,
+                'accessLevel'   => 1,
+            ])->id;
+        },
+        'isSurvey'      => true
+    ];
+});
+
 $factory->define(App\Models\Recipient::class, function (Faker\Generator $faker) use ($factory) {
     return [
         'ownerId'   => 1,
         'name'      => $faker->name(),
         'mail'      => $faker->safeEmail(),
         'position'  => $faker->jobTitle(),
+    ];
+});
+
+$factory->define(App\Models\Survey::class, function (Faker\Generator $faker) use ($factory) {
+    return [
+        'name' => $faker->words(3, true),
+        'type' => 'App\SurveyTypes::Individual',
+        'lang' => 'en',
+        'invitationTextId' => $factory->create(App\Models\EmailText::class)->id,
+        'ownerId' => 1,
+        'startDate' => Carbon\Carbon::now(),
+        'endDate' => Carbon\Carbon::now()->addDay(30),
+        'description' => $faker->words(15, true),
+        'thankYouText' => $faker->words(15, true),
+        'questionInfoText' => $faker->words(15, true),
+    ];
+});
+
+$factory->define(App\Models\SurveyQuestionCategory::class, function (Faker\Generator $faker) use ($factory) {
+    return [
+        'surveyId'      => function() {
+            return factory(App\Models\Survey::class)->create()->id;
+        },
+        'categoryId'    => function() {
+            return factory(App\Models\QuestionCategory::class)->create()->id;
+        },
+        'order'         => 0,
     ];
 });
 
